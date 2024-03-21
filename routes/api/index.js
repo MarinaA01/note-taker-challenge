c1b6cdff07f656e7c801edc257c3a0dfb166701b
Marina <marinajalonzo1@gmail.com>
2024-03-21 18:25:41 -0500

    Routes fixes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 
 // Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes');
-const indexRouter = require('./index');
 
-router.use('/notes', notesRouter);
-router.use('/index', indexRouter);
+router.use('./notes', notesRouter);
 
 module.exports = router;
